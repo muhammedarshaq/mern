9d886dafd5cb31da1a3bf6334a6e149c12a97e3e
ΛRSHΛQ SHΛZLY <mjmarshaq@gmail.com>
2023-12-03 22:46:30 +0530

    Post model, create post done

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-// @route   GET  api/posts
-// @desc    Test route
-// @access  public 
-router.get('/', (req, res) => res.send('Post route'));
+// Models
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required!").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // Getting the user who creates the post
+      const user = await User.findById(req.user.id).select("-password");
+
+      // Creating new post
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      // Save the post in DB and set that post in variable
+      const post = await newPost.save();
+
+      // Get the saved post as response
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error!");
+    }
+  }
+);
 
 module.exports = router;
